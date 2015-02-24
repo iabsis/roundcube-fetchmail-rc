152327f979d9599e22a5880f3fedfd1c98d1e8df
Olivier Bitsch <olivier.bitsch@gmail.com>
2015-02-24 11:26:45 +0100

    * Add new line.

--- config.inc.php
+++ config.inc.php
@@ -6,6 +6,7 @@
 
 // Root target of your roundcube installation
 $rcmail_config['fetchmail_rc_roundcube_install_path'] = __DIR__ . "/../../" ;
+
 // Error count before to warn the user by mail that his mailbox is not working anymore
 $rcmail_config['fetchmail_rc_errors_before_warning'] = 5;
 
