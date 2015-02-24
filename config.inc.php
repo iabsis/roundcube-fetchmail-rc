2e8052c7c61a5da2445eabb0c1e1ca2512a6cb69
gilles hemmerle <gilles.h@iabsis.com>
2015-02-24 11:15:23 +0100

    Changed default roundcube install folder
    Changed the default installation folder set in the config file in order
    to fit all standard roundcube installs

--- config.inc.php
+++ config.inc.php
@@ -5,7 +5,7 @@
  */
 
 // Root target of your roundcube installation
-$rcmail_config['fetchmail_rc_roundcube_install_path'] = "/usr/share/igestis/modules/Roundcube/roundcubemail/";
+$rcmail_config['fetchmail_rc_roundcube_install_path'] = __DIR__ . "/../../" ;
 // Error count before to warn the user by mail that his mailbox is not working anymore
 $rcmail_config['fetchmail_rc_errors_before_warning'] = 5;
 
